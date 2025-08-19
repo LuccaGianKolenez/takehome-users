87ad74ed82eb815b5f988f5bb83b4c6f5f1e69b1
Lucca Kolenez <lkolenez@gmail.com>
2025-08-19 11:20:49 -0300

    feat(web): listagem de usuários com React Query

--- apps/web/src/components/users-list.tsx
+++ apps/web/src/components/users-list.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useQuery } from '@tanstack/react-query';
-import { api } from './lib/api';
+import { api } from '../app/lib/api';
 import Link from 'next/link';
 
 type User = { id:string; name:string; email:string };
